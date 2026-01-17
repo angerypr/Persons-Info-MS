2c4e84bbe1762d85e68eee8ca0d1756966d3f79d
angerypr <angerypr16@gmail.com>
2026-01-17 17:26:11 -0400

    Creating database connection and structure

--- Microservices/MS2-Manager/src/server.ts
+++ Microservices/MS2-Manager/src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 app.get("/health", (_, res) => {
   res.json({ status: "MS2 Manager is running" });
 });
 
-app.listen(3000, () => {
-  console.log("MS2 Manager running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`MS2 Manager running on port ${process.env.PORT}`);
 });
